5a7a5bffc2b291357e75180f8fbbaea1b609fcf2
parth9784 <pdsharma458@gmail.com>
2024-06-27 23:43:39 +0530

    removed some unused lines of code and takled warnings

--- src/cardlist.js
+++ src/cardlist.js
@@ -1,14 +1,11 @@
 import React  from "react";
 import Newscard from "./newscard";
 export default function Cardlist({detail}){
-    // console.log(detail)
-
     return(
         <div className="flex flex-wrap gap-3 px-7 justify-around">
           {
             detail.map((i)=>{
-                if(!i.urlToImage){return}
-                return <Newscard url={i.urlToImage} desc={i.description} title={i.title} at={i.source.name} urlto={i.url} />
+                return(<Newscard url={i.urlToImage} desc={i.description} title={i.title} at={i.source.name} urlto={i.url} />);
             })
         }
         </div>
