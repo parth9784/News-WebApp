f14a469d27e9d4c902a4f36f1527b5a10efc0708
Parthdadhich458 <114915077+parth9784@users.noreply.github.com>
2024-07-18 14:24:35 +0530

    Update Nodata.js added reload

--- src/Nodata.js
+++ src/Nodata.js
@@ -5,8 +5,7 @@ export default function NotFound() {
     const navigate = useNavigate();
 
     function handleClick() {
-        console.log("clicked home");
-        navigate('/');
+         window.location.reload();
     }
 
     return (
